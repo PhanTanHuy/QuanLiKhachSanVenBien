e5ebc4ca121a81437b31eeafef2c1253b5ef56d9
PhanTanHuy <phantanhuy265@gmail.com>
2025-10-21 12:45:34 +0700

    add route va controller vi du

---
+++ backend/controllers/homeController.js
@@ -0,0 +1,3 @@
+exports.getHome = (req, res) => {
+    res.send('Đây là trang chủ!');
+};
--- backend/sever.js
+++ backend/sever.js
@@ -1,14 +1,16 @@
 require('dotenv').config(); 
 const express = require('express');
 require('./configs/database'); 
+// const accountRoutes = require('./routes/accountRoutes');
+const homeRoutes = require('./routes/homeRoutes');
 
-
+// Routes khi truy cập trinh duyệt thì trả về / trống (vd trang admin trả về /admin)
+// route làm gì? route sẽ định nghĩa truy cập vào cái gì trên trình duyệt thì trả về hàm trong controller tương ứng (cái này mọi người hỏi chatgpt để hiểu)
 const app = express();
 app.use(express.json());
-// connectDB(); 
-app.get('/', (req, res) => {
-    res.send('Server và Database đang chạy!');
-});
+// app.use('/accounts', accountRoutes);
+app.use('/', homeRoutes);
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server chạy trên http://localhost:${PORT}`);
