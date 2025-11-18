0979e9e46e9f728a1dd1931a1ad0ee39195a53f3
Lamquocviet <lamquocviet2005.nvt@gmail.com>
2025-11-18 15:34:57 +0700

    BE_login

---
+++ backend/routes/userRpute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const {authMe} = require("../controllers/userController.js");
+
+
+
+const router = express.Router();
+router.get("/me", authMe);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,8 @@ const homeRoutes = require('./routes/homeRoutes');
 const adminRoutes = require('./routes/admin/adminRoutes');
 const authRoute = require('./routes/authRoute.js');
 const cookiesParser = require('cookie-parser');
-
+const { protectedRoute } = require('./middlewares/authMiddleware.js');
+const userRpute = require('./routes/userRpute.js');
 
 
 const app = express();
@@ -15,7 +16,11 @@ app.use(cookiesParser());
 //load aset
 app.use(express.static(path.join(__dirname, '../frontend')));
 app.use('/admin', adminRoutes);
+
 app.use("/api/auth", authRoute);
+//private routes
+app.use(protectedRoute)
+app.use("/api/users", userRpute)
 
 const PORT = 3000;
 app.listen(PORT, () => {
